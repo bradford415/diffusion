b91152acc4fc100da50f50dfb6d704325f7e093d
Bradley Selee <bselee415@gmail.com>
2024-09-14 11:32:02 -0400

    Implemented mha

--- diffusion/models/layers.py
+++ diffusion/models/layers.py
@@ -5,38 +5,6 @@ from torch import nn
 from torch.nn import function as F
 
 
-class Attention(nn.Module):
-    """TODO"""
-
-    def __init__(self, dropout=0.0):
-        """TODO"""
-        super().__init__()
-        self.dropout = dropout
-
-
-class SelfAttention(nn.Module):
-    """TODO"""
-
-    def __init__(self, dim, heads, dim_head, num_mem_kv):
-        """dim"""
-        super().__init__()
-        self.heads = heads
-        scale
-
-    def forward(self, q, k, v):
-        """TODO
-        q: queries (batch_size, q_len, d_model)
-        k: keys (batch_size, q_len, d_model)
-        v: vectors (batch_size, q_len, d_model)
-
-        """
-
-        q_len, k_len = q.shape[-1], k.shape[-1]
-        # Start here and implement attention https://github.com/sooftware/attentions/blob/master/attentions.py
-
-        torch.matmul
-
-
 class MultiheadAttention(nn.Module):
     """TODO"""
 
@@ -47,6 +15,9 @@ class MultiheadAttention(nn.Module):
             embed_dim: Total dimension of the model; embed_dim will be split across
                        num_heads (attention_dim // num_heads)  after it's projected
             num_heads: Number of attention heads; each head will have dimension of attention_dim // num_heads
+        
+        Returns:
+            Linear projected attention values (batch, seq_len, embed_dim)
         """
         assert (
             embed_dim % num_heads == 0
@@ -61,6 +32,8 @@ class MultiheadAttention(nn.Module):
         self.v_proj = nn.Linear(embed_dim, embed_dim, bias=False)
 
         self.attention = Attention()
+
+        self.linear_proj = nn.linear(embed_dim, embed_dim, bias=False)
 
         # self.heads = nn.ModuleList(
         #     [Attention(head_dim, head_size, block_size) for _ in range(num_heads)]
@@ -77,20 +50,36 @@ class MultiheadAttention(nn.Module):
                     as q & v
         """
         # Linearly project q, k, & v (batch, seq_len, embed_dim)
-        query = self.q_proj(queries)
-        key = self.k_proj(keys)
-        value = self.v_proj(values)
+        queries = self.q_proj(queries)
+        keys = self.k_proj(keys)
+        values = self.v_proj(values)
 
         # Split into heads (batch, num_heads, seq_len, head_dim)
-        query_heads = query.view(
-            query.shape[0], query.shape[1], self.num_heads, self.head_dim
+        query_heads = queries.view(
+            queries.shape[0], queries.shape[1], self.num_heads, self.head_dim
         ).transpose(1, 2)
-        key_heads = key.view(
-            key.shape[0], key.shape[1], self.num_heads, self.head_dim
+        key_heads = keys.view(
+            keys.shape[0], keys.shape[1], self.num_heads, self.head_dim
         ).transpose(1, 2)
-        value_heads = value.view(
-            value.shape, value.shape[1], self.num_heads, self.head_dim
+        value_heads = values.view(
+            values.shape, values.shape[1], self.num_heads, self.head_dim
         ).transpose(1, 2)
+
+        # Compute attention on all heads
+        attention = self.attention(query_heads, key_heads, value_heads)
+
+        # Combine all the heads together (concatenation step);
+        # (b, heads, seq, head_dim) -> (b, seq, heads, head_dim) -> (b, seq, heads*head_dim)
+        attention = (
+            attention.transpose(1, 2)
+            .contiguous()
+            .view(attention.shape[0], -1, self.num_heads * self.head_dim)
+        )
+
+        # Final linear projection of MHA
+        attention_proj = self.linear_proj(attention)
+
+        return attention_proj
 
 
 class Attention(nn.Module):
@@ -99,23 +88,20 @@ class Attention(nn.Module):
     into multiple heads
     """
 
-    def __init__(self, attention_dim: int):
-        """Initialize attention module
-
-        Args:
-            attention_dim: Dimension of the attention 
-        """
+    def __init__(self):
+        """Initialize attention module"""
         super().__init__()
 
-        # Used to scale the qk dot product
-        self.sqrt_dim = torch.sqrt(attention_dim)
-
     def forward(
-        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: Optional[torch.Tensor] = None
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        mask: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         """Compute attention on q, k, & v
 
-        q, k, v should be a minimum of 3 dimensions such as (batch, seq_len, embed_dim) 
+        q, k, v should be a minimum of 3 dimensions such as (batch, seq_len, embed_dim)
         or (batch, num_heads, seq_len, head_dim); attention will be computed on the last 2 dims
 
         Args:
@@ -128,8 +114,12 @@ class Attention(nn.Module):
         Returns:
            The context vectors (batch_size, seq_len, d_model)
         """
-        # (batch_size, q_len, k_len)
-        scores = torch.matmul(q, k.transpose(-2, -1)) / self.sqrt_dim
+
+        # Used to scale the qk dot product
+        sqrt_dim = torch.sqrt(k.shape[-1])
+
+        # (batch_size, num_heads, q_len, k_len)
+        scores = torch.matmul(q, k.transpose(-2, -1)) / sqrt_dim
 
         # Mask attention indices if mask is provided; softmax will set -inf to 0
         if mask is not None:
@@ -139,9 +129,9 @@ class Attention(nn.Module):
 
         # Considered the context vectors because it's a weighted sum of the attention scores;
         # this gives a `context` value about an input's location
-        context = torch.matmul(attention, v)  # (batch_size, v_len, d_model)
+        context = torch.matmul(attention, v)  # (batch_size, num_heads, v_len, head_dim)
         return context
-    
+
 
 class AttentionFeatureMaps(nn.Module):
     # START HERE try to implement as shown here
@@ -155,7 +145,7 @@ class AttentionFeatureMaps(nn.Module):
         """Initialize attention module
 
         Args:
-            attention_dim: Dimension of the attention 
+            attention_dim: Dimension of the attention
         """
         super().__init__()
 
@@ -163,11 +153,15 @@ class AttentionFeatureMaps(nn.Module):
         self.sqrt_dim = torch.sqrt(attention_dim)
 
     def forward(
-        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: Optional[torch.Tensor] = None
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        mask: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         """Compute attention on q, k, & v
 
-        q, k, v should be a minimum of 3 dimensions such as (batch, seq_len, embed_dim) 
+        q, k, v should be a minimum of 3 dimensions such as (batch, seq_len, embed_dim)
         or (batch, num_heads, seq_len, head_dim); attention will be computed on the last 2 dims
 
         Args:
