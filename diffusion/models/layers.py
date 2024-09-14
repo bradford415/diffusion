761bcede3de33227d0977ad83cfbed565204441a
Bradley Selee <bselee415@gmail.com>
2024-09-14 13:12:28 -0400

    Implemented mha for feature maps

--- diffusion/models/layers.py
+++ diffusion/models/layers.py
@@ -6,14 +6,17 @@ from torch.nn import function as F
 
 
 class MultiheadAttention(nn.Module):
-    """TODO"""
+    """TODO
+    
+    Implementation based on: https://github.com/hkproj/pytorch-transformer/blob/3beddd2604bfa5e0fbd53f52361bd887dc027a8c/model.py#L83
+    """
 
     def __init__(self, embed_dim, num_heads):
         """TODO
 
         Args:
             embed_dim: Total dimension of the model; embed_dim will be split across
-                       num_heads (attention_dim // num_heads)  after it's projected
+                       num_heads (embed_dim // num_heads)  after it's projected
             num_heads: Number of attention heads; each head will have dimension of attention_dim // num_heads
         
         Returns:
@@ -83,9 +86,12 @@ class MultiheadAttention(nn.Module):
 
 
 class Attention(nn.Module):
-    """Scaled dot product attention with an optional mask; typically this
-    is used in MHA where q, k, v have already been linearly projected and split
-    into multiple heads
+    """Scaled dot product attention with an optional mask
+    
+    Typically this is used in MHA where q, k, v have already been linearly 
+    projected and split into multiple heads; this can be used for sequences 
+    and feature maps (h, w) but the feature maps features (pixels) should be 
+    flattened
     """
 
     def __init__(self):
@@ -133,57 +139,98 @@ class Attention(nn.Module):
         return context
 
 
-class AttentionFeatureMaps(nn.Module):
-    # START HERE try to implement as shown here
-    # https://github.com/w86763777/pytorch-ddpm/blob/master/diffusion.py
-    """Scaled dot product attention with an optional mask; typically this
-    is used in MHA where q, k, v have already been linearly projected and split
-    into multiple heads
+class MHAFeatureMaps(nn.Module):
+    """TODO
+    
+    This implementation is based on: https://github.com/w86763777/pytorch-ddpm/blob/f804ccbd58a758b07f79a3b9ecdfb1beb67258f6/model.py#L78
+    but modified to use multiple heads
     """
 
-    def __init__(self, attention_dim: int):
-        """Initialize attention module
+    def __init__(self, embed_ch, num_heads):
+        """TODO
 
         Args:
-            attention_dim: Dimension of the attention
+            embed_ch: Total channels of the model; embed_ch will be split across
+                       num_heads (embed_ch // num_heads) after it's projected
+            num_heads: Number of attention heads; each head will have dimension 
+                       of embed_ch // num_heads
+        
+        Returns:
+            Linear projected attention values (batch, seq_len, embed_dim)
         """
-        super().__init__()
+        assert (
+            embed_ch % num_heads == 0
+        ), "The number of heads should be divisble by the attenion_dim"
 
-        # Used to scale the qk dot product
-        self.sqrt_dim = torch.sqrt(attention_dim)
+        self.num_heads = num_heads
 
-    def forward(
-        self,
-        q: torch.Tensor,
-        k: torch.Tensor,
-        v: torch.Tensor,
-        mask: Optional[torch.Tensor] = None,
-    ) -> torch.Tensor:
-        """Compute attention on q, k, & v
+        self.head_dim = embed_ch // num_heads
 
-        q, k, v should be a minimum of 3 dimensions such as (batch, seq_len, embed_dim)
-        or (batch, num_heads, seq_len, head_dim); attention will be computed on the last 2 dims
+        self.group_norm = nn.GroupNorm(32, embed_ch)
+
+        # NOTE: might need to make bias true?
+        self.q_proj = nn.Conv2d(embed_ch, embed_ch, kernel_size=1, stride=1, padding=1, bias=False)
+        self.k_proj = nn.Conv2d(embed_ch, embed_ch, kernel_size=1, stride=1, padding=1, bias=False)
+        self.v_proj = nn.Conv2d(embed_ch, embed_ch, kernel_size=1, stride=1, padding=1, bias=False)
+
+        self.attention = Attention()
+
+        self.final_proj = nn.Conv2d(embed_ch, embed_ch, bias=False)
+
+        # self.heads = nn.ModuleList(
+        #     [Attention(head_dim, head_size, block_size) for _ in range(num_heads)]
+        # )
+
+    def forward(self, queries: torch.Tensor, keys: torch.Tensor, values: torch.Tensor):
+        """ "Forward pass through Multiheaded Attention;
+        for self-attention the queries, keys, and values should be the same
 
         Args:
             queries: Input tensor to compute the attention of
             keys: Input tensor to compute the attention of
             values: Input tensor to compute the context of; for self-attention this should be the same
                     as q & v
-            mask: Optional tensor containing indices to be masked; typically used in decoders for NLP
-
-        Returns:
-           The context vectors (batch_size, seq_len, d_model)
         """
-        # (batch_size, q_len, k_len)
-        scores = torch.matmul(q, k.transpose(-2, -1)) / self.sqrt_dim
+        # Project q, k, & v (batch, embed_ch, height, width)
+        queries = self.q_proj(queries)
+        keys = self.k_proj(keys)
+        values = self.v_proj(values)
 
-        # Mask attention indices if mask is provided; softmax will set -inf to 0
-        if mask is not None:
-            scores.masked_fill_(mask.view(scores.size()), -float("Inf"))
+        batch, embed_ch, height, width  = queries.shape
 
-        attention = F.softmax(scores, dim=-1)
+        # Flatten spatial features;
+        # (batch, height, width, embed_ch) -> (batch, height*width, embed_ch)
+        queries = queries.permute(0, 2, 3, 1).view(batch, height*width, embed_ch)
+        keys = keys.permute(0, 2, 3, 1).view(batch, height*width, embed_ch)
+        values = values.permute(0, 2, 3, 1).view(batch, height*width, embed_ch)
 
-        # Considered the context vectors because it's a weighted sum of the attention scores;
-        # this gives a `context` value about an input's location
-        context = torch.matmul(attention, v)  # (batch_size, v_len, d_model)
-        return context
+        # Split into multiple heads (batch, num_heads, features, head_dim)
+        query_heads = queries.view(
+            queries.shape[0], queries.shape[1], self.num_heads, self.head_dim
+        ).transpose(1, 2)
+        key_heads = keys.view(
+            keys.shape[0], keys.shape[1], self.num_heads, self.head_dim
+        ).transpose(1, 2)
+        value_heads = values.view(
+            values.shape, values.shape[1], self.num_heads, self.head_dim
+        ).transpose(1, 2)
+
+        # Compute attention on all heads
+        attention = self.attention(query_heads, key_heads, value_heads)
+
+        # Combine all the heads together (concatenation step);
+        # (b, heads, features, head_dim) -> (b, features, heads, head_dim) -> (b, features, heads*head_dim)
+        attention = (
+            attention.transpose(1, 2)
+            .contiguous()
+            .view(attention.shape[0], -1, self.num_heads * self.head_dim)
+        )
+
+        # Convert features back to spatial dimensions
+        # (b, height, width, embed_ch) -> (b, embed_ch, height, width)
+        attention = attention.view(batch, height, width, embed_ch).permute(0, 3, 1, 2)
+
+        # Final projection of MHA
+        attention_proj = self.final_proj(attention)
+
+        return attention_proj
